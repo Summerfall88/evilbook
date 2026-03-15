90ac75d68df2ce4fdc34ba4e6c36aea9268d4ca0
Summerfall88 <125675029+Summerfall88@users.noreply.github.com>
2026-03-15 09:03:27 +0500

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import { usePWAInstall } from "@/hooks/usePWAInstall";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { useAuth } from "@/hooks/useAuth";
